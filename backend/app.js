2c1378f4c7083f6d0cb8cc8d79b46a7b5666b649
cindyoshita <cindynat11@gmail.com>
2022-08-20 12:36:40 -0300

    Apostila7_mean
    - reorganizado os arquivos do backend

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Cliente = require('./models/cliente');
+const clienteRoutes = require ('./rotas/clientes');
 
 const {
   MONGODB_USER,
@@ -20,21 +20,6 @@ mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_CL
 });
 app.use(bodyParser.json());
 
-const clientes = [
-  {
-    id: '1',
-    nome: 'José',
-    fone: '11223344',
-    email: 'jose@email.com'
-  },
-  {
-    id:'2',
-    nome: 'Jaqueline',
-    fone: '22112211',
-    email: 'jaqueline@email.com'
-  }
-]
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', "*");
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -42,70 +27,6 @@ app.use((req, res, next) => {
   next();
 })
 
-app.post('/api/clientes', (req, res, next) => {
-  const cliente = new Cliente({
-    nome:req.body.nome,
-    fone:req.body.fone,
-    email:req.body.email,
-  })
-  cliente.save().
-  then (clienteInserido => {
-  res.status(201).json({
-  mensagem: 'Cliente inserido',
-  id: clienteInserido._id
-  })
-  })
- 
-});
-
-
-app.get('/api/clientes', (req, res, next) => {
-  Cliente.find().then(documents => {
-  console.log (documents)
-  res.status(200).json({
-  mensagem: "Tudo OK",
-  clientes: documents
-  });
-  })
- });
-
- app.get('/api/clientes/:id', (req, res, next) => {
-  Cliente.findById(req.params.id).then(cli => {
-  if (cli){
-  res.status(200).json(cli);
-  }
-  else
-  res.status(404).json({mensagem: "Cliente não encontrado!"})
-  })
- });
- 
-
- app.delete ('/api/clientes/:id', (req, res, next) => {
-  Cliente.deleteOne ({_id: req.params.id}).then((resultado) => {
-  console.log (resultado);
-  res.status(200).json({mensagem: "Cliente removido"})
-  });
- });
-
- app.put ("/api/clientes/:id", (req, res, next) => {
-  const cliente = new Cliente({
-  _id: req.params.id,
-  nome: req.body.nome,
-  fone: req.body.fone,
-  email: req.body.email
-  });
-  Cliente.updateOne({_id: req.params.id}, cliente)
-  .then ((resultado) => {
-  console.log (resultado)
-  });
-  res.status(200).json({mensagem: 'Atualização realizada com sucesso'})
- });
-
-app.use('/api/clientes', (req, res, next) => {
-  res.status(200).json({
-    mensagem: 'Tudo OK',
-    clientes: clientes
-  });
-});
+app.use ('/api/clientes', clienteRoutes);
 
 module.exports = app;
